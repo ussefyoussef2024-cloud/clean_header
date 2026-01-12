f958575ce39d6d33402de34df8aaf91d53baebd0
ussefyoussef2024-cloud <ussefyoussef2024@gmail.com>
2026-01-12 21:25:48 +0100

    Refactor DOMContentLoaded event listener setup

--- script.js
+++ script.js
@@ -258,7 +258,7 @@ document.addEventListener('DOMContentLoaded', () => {
     function transformMessageId(value) {
         if (value.includes('<') && value.includes('>')) {
             return value.replace(/<([^@]+)@([^>]+)>/, (match, localPart, domain) => {
-                return `<[IDmailer]_id_[ID]-f_${localPart}@${domain}>`;
+                return `<${localPart}[EID]@${domain}>`;
             });
         }
         return value;
@@ -276,5 +276,3 @@ document.addEventListener('DOMContentLoaded', () => {
         return text;
     }
 });
-
-
